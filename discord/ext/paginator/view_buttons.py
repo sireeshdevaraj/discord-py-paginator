6d3012b9c12d42d0fd6665603a44eb4c2f0d1a4c
KūHaku <65805722+sireeshdevaraj@users.noreply.github.com>
2022-04-29 13:32:54 +0530

    Update view_buttons.py

--- discord/ext/paginator/view_buttons.py
+++ discord/ext/paginator/view_buttons.py
@@ -181,7 +181,7 @@ class Stop(button.BetterButton):
 
     async def on_click(self, interaction: Interaction):
         await self.parent.paginator_stop(interaction)
-        await interaction.delete_original_message()
+        await self.parent.delete_original_message(interaction)
 
 class Start(button.BetterButton):
     def __init__(
